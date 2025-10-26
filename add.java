3874b710d53bf7022b8024f562a5095321930ff1
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-26 12:15:28 +0530

    2

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 
 public class add{
   public static void main(String[] args) {
-    System.out.println("12345__12345");
+    System.out.println("12345__123456");
     System.out.println("101236");
   }
 }
