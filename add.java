9d05f439ea47da1ebb601a1df877acaf98deab17
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-26 12:04:56 +0530

    new cherry

--- add.java
+++ add.java
@@ -1,9 +1,14 @@
 
 public class add{
   public static void main(String[] args) {
+<<<<<<< HEAD
     System.out.println("12345__1235");
     System.out.println("1012");
     System.out.println("12345__1234");
     System.out.println("10123");
+=======
+    System.out.println("12345__12345");
+    System.out.println("101236");
+>>>>>>> fe469c7 (cherry)
   }
 }
